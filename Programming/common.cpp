fe5f6da84e3f9c750163033d1eb8a2b4eb7d8ace
AndiH <a.herten+github@gmail.com>
2015-04-16 14:57:01 +0200

    New commons

--- Programming/common.cpp
+++ Programming/common.cpp
@@ -29,7 +29,10 @@
 #include "tableau_colors.cpp"
 
 namespace andi {  // everything is in a name space to structure it better
-
+	// some constants
+	double crossSig = 0.1; // mu barn
+	double crossBkg = 45737; // mu barn
+	double brSig = 0.0931;
 	/**
 	 * @brief Set my custom ROOT canvas style
 	 * @details Gets rid of the margins and sets some other stuff
@@ -212,6 +215,9 @@ namespace andi {  // everything is in a name space to structure it better
 		pt->AddText(histTitle);
 		return pt;
 	}
+	/**
+	 * @brief TObject version of the last one
+	 **/
 	TPaveText * makePadTitle(TObject * obj, double rightBorder = 0.96) {
 		// canvas->SetTopMargin(0.075);
 		TPaveText *pt = new TPaveText(0.16, 0.932, rightBorder, 0.99, "NDC");
@@ -329,6 +335,14 @@ namespace andi {  // everything is in a name space to structure it better
 		legend->SetBorderSize(1);
 		return legend;
 	}
+	/**
+	 * @brief Converts an array of histograms into a THStack
+	 * @details Gives the stack proper name, tile, and axes on the way, so it can be manipulated as a usual ROOT object (because usually, it can't, as the stuff is only defined after drawing)
+	 * 
+	 * @param histos TObjArray with histograms. Take care of the order!
+	 * @param stackAddOption Optional string which is used when stack->Add(hist, option)
+	 * @return THStack of histograms
+	 */
 	THStack * histogramsToStack(TObjArray * histos, TString stackAddOption = "") {
 		THStack * myStack = new THStack();
 		int nHistograms = histos->GetEntries();
@@ -349,8 +363,18 @@ namespace andi {  // everything is in a name space to structure it better
 		return myStack;
 	}
 
-	// TLegend * stackLegend(THStack * stack, TString header = "Histograms", TLegend * baseLegend) 
-
+	/**
+	 * @brief Creates a legend for stack
+	 * @details Make sure the histograms in the stack have proper and short titles, as those are used for the legend
+	 * 
+	 * @param stack A stack of histograms
+	 * @param header The header of the legend
+	 * @param height y lower position of box
+	 * @param width x lower position of box
+	 * @param y2 y upper position of box
+	 * 
+	 * @return A legend. Dont' forget to Draw()
+	 */
 	TLegend * stackLegend(THStack * stack, TString header = "Histograms", double height = 0.75, double width = 0.7, double y2 = 0.93) {
 		TLegend * tempLegend = andi::plainLegend(width, height, 0.96, y2);
 		tempLegend->SetHeader(header);
@@ -409,13 +433,37 @@ namespace andi {  // everything is in a name space to structure it better
 		TF1 * fitPre1 = new TF1("fitPre1", "gaus", -innerRangeMax, innerRangeMax);
 		hist->Fit(fitPre1, "Q0R");
 		fitPre1->GetParameters(&parameters[0]);
-std::cout << parameters[0] << ", " << parameters[3] << std::endl;
+// std::cout << parameters[0] << ", " << parameters[3] << std::endl;
 		TF1 * fitPre2 = new TF1("fitPre2", "gaus", -outerRangeMax, outerRangeMax);
 		hist->Fit(fitPre2, "Q0R");
 		fitPre2->GetParameters(&parameters[3]);
-std::cout << parameters[0] << ", " << parameters[3] << std::endl;
 		// startParameters = &parameters;
-		return doubleGaussFit(hist, verbose, outerRange, parameters);
+		return doubleGaussFit(hist, verbose, -outerRangeMax, outerRangeMax, parameters);
+
+	}
+	/**
+	 * @brief Does the as doubleGaussFit but does not expected 0-centered distribution
+	 * @details Instead of choosing the range symmetrically around zero, the histogram's main is retrieved as the central value
+	 */
+	TF1 * doubleGaussFitNonZero(TH1 * hist, bool verbose = false, bool useAutoRange = true, double innerRangeMax = 0.05, double outerRangeMax = 0.3) {
+		double centralValue = hist->GetMean();
+		Double_t parameters[6] = {0, 0, 0, 0, 0, 0};
+		gStyle->SetOptFit(1);
+		if (useAutoRange) {
+			innerRangeMax = fabs(centralValue - hist->GetXaxis()->GetXmax()) / 10;
+			outerRangeMax = fabs(centralValue - hist->GetXaxis()->GetXmax()) / 10 * 8;
+		}
+		// std::cout << "centralValue: " << centralValue << std::endl;
+		// std::cout << "innerRangeMax: " << innerRangeMax << std::endl;
+		// std::cout << "outerRangeMax: " << outerRangeMax << std::endl;
+		TF1 * fitPre1 = new TF1("fitPre1", "gaus", centralValue - innerRangeMax, centralValue + innerRangeMax);
+		hist->Fit(fitPre1, "Q0R");
+		fitPre1->GetParameters(&parameters[0]);
+		TF1 * fitPre2 = new TF1("fitPre2", "gaus", centralValue - outerRangeMax, centralValue + outerRangeMax);
+		hist->Fit(fitPre2, "Q0R");
+		fitPre2->GetParameters(&parameters[3]);
+		// startParameters = &parameters;
+		return doubleGaussFit(hist, verbose, centralValue - outerRangeMax, centralValue + outerRangeMax, parameters);
 
 	}
 	/**
@@ -428,8 +476,8 @@ std::cout << parameters[0] << ", " << parameters[3] << std::endl;
 	 * @param startParameters An array of six values from where to start the minimization
 	 * @return Pointer to TF1 with two Gaussians. See description of other function.
 	 */
-	TF1 * doubleGaussFit(TH1 * hist, bool verbose, double outerRange, double * startParameters) {
-		TF1 * fitProper = new TF1("fitProper", "gaus(0)+gaus(3)", -outerRange, outerRange);
+	TF1 * doubleGaussFit(TH1 * hist, bool verbose, double rangeStart, double rangeEnd, double * startParameters) {
+		TF1 * fitProper = new TF1("fitProper", "gaus(0)+gaus(3)", rangeStart, rangeEnd);
 		fitProper->SetParameters(startParameters);
 		fitProper->SetParName(0, "Const (inner)");
 		fitProper->SetParName(1, "Mean (inner)");
@@ -448,11 +496,9 @@ std::cout << parameters[0] << ", " << parameters[3] << std::endl;
 			std::cout << "  sigma (outer) = " << fitProper->GetParameter(5) << " pm " << fitProper->GetParError(5) << std::endl;
 		}	
 
-		// fitProper->GetParameters(&parameters[0]);
 		fitProper->SetLineColor(hist->GetLineColor());
 		fitProper->SetLineWidth(hist->GetLineWidth());
 		fitProper->SetLineStyle(2);
-		std::cout << fitProper->GetParameter(1) << std::endl;
 
 		return fitProper;
 	}
@@ -487,26 +533,41 @@ std::cout << parameters[0] << ", " << parameters[3] << std::endl;
 	 * @param fit Should there be a Gaussian fit?
 	 * @return TCanvas point. You dont need to use it, but if you want, whoops, here it is.
 	 */
-	TCanvas * createCanvasDrawAndSave(TH1 * h, TString filename, TString basename, bool save = true, bool fit = false) {
+	TCanvas * createCanvasDrawAndSave(TH1 * h, TString filename, TString basename, bool save = true, int fit = 0) {
 		TString canvasName = h->GetName();
 		canvasName.Remove(0, 1);
 		canvasName.Prepend("c");
 		TCanvas * c = new TCanvas(canvasName, filename, 0, 0, 800, 500);
 		h->Draw("HIST");
-		if (fit) {
+		if (fit == 1) {
 			TF1 * gf = gaussFit(h, true);
+			gf->Draw("SAME");
+		} else if (fit == 2) {
+			TF1 * gf = doubleGaussFitNonZero(h, true);
 			gf->Draw("SAME");
 		}
 		andi::makePadTitleAndDraw(h);
+		if (fit == 2) {
+			shrinkStatBox(h, 1.3);
+		}
 		if (save) andi::saveCanvas(c, basename);
 		return c;
 	}
 	/**
-	 * @brief See andi::createCanvasDrawAndSave, but this version with default fitting enabled
+	 * @brief See andi::createCanvasDrawAndSave. This version fits a Gauss
 	 */
-	TCanvas * createCanvasFitDrawAndSave(TH1 * h, TString filename, TString basename, bool save = true, bool fit = true) {
-		return createCanvasDrawAndSave(h, filename, basename, save, true);
+	TCanvas * createCanvasFitDrawAndSave(TH1 * h, TString filename, TString basename, bool save = true, int fit = 1) {
+		return createCanvasDrawAndSave(h, filename, basename, save, 1);
 	}
+	/**
+	 * @brief See andi::createCanvasDrawAndSave. This version fits a double Gauss.
+	 */
+	TCanvas * createCanvasFitDoubleDrawAndSave(TH1 * h, TString filename, TString basename, bool save = true, int fit = 2) {
+		return createCanvasDrawAndSave(h, filename, basename, save, 2);
+	}
+	/**
+	 * @brief See andi::createCanvasDrawAndSave. For TH2, moved the Z axis to the left as well.
+	 */
 	TCanvas * createCanvasDrawAndSave(TH2 * h, TString filename, TString basename, bool save = true) {
 		TString canvasName = h->GetName();
 		canvasName.Remove(0, 1);
@@ -517,7 +578,23 @@ std::cout << parameters[0] << ", " << parameters[3] << std::endl;
 		if (save) andi::saveCanvas(c, basename);
 		return c;
 	}
-	TCanvas * createCanvasDrawAndSave(THStack * stack, TString filename, TString basename, bool save = true, TString stackTitle = "Histograms") {
+	/**
+	 * @brief See andi::createCanvasDrawAndSave. For everything which comes from TObject. Like TGraph and sorts.
+	 */
+	TCanvas * createCanvasDrawAndSave(TObject * h, TString filename, TString basename, bool save = true, TString drawParams = "") {
+		TString canvasName = h->GetName();
+		canvasName.Remove(0, 1);
+		canvasName.Prepend("c");
+		TCanvas * c = new TCanvas(canvasName, filename, 0, 0, 800, 500);
+		h->Draw(drawParams);
+		andi::makePadTitleAndDraw(h);
+		if (save) andi::saveCanvas(c, basename);
+		return c;
+	}
+	/**
+	 * @brief See andi::createCanvasDrawAndSave. For THStack. Also creates a legend.
+	 */
+	TCanvas * createCanvasDrawAndSave(THStack * stack, TString filename, TString basename, bool save = true, TString stackTitle = "Histograms", double boxStartY = 0.7, double boxStartX = 0.7) {
 		TString canvasName = stack->GetName();
 		canvasName.Remove(0, 1);
 		canvasName.Prepend("c");
@@ -526,7 +603,7 @@ std::cout << parameters[0] << ", " << parameters[3] << std::endl;
 		stack->GetHistogram()->SetStats(false);
 		stack->Draw("NOSTACK");
 		andi::makePadTitleAndDraw((TH1*)stack);
-		TLegend * leg = andi::stackLegend(stack, stackTitle, 0.7);
+		TLegend * leg = andi::stackLegend(stack, stackTitle, boxStartY, boxStartX);
 		leg->Draw();
 		if (save) andi::saveCanvas(c, basename);
 		return c;
@@ -545,6 +622,110 @@ std::cout << parameters[0] << ", " << parameters[3] << std::endl;
 		TFileCollection collection("somename", "", fileName);
 		tempChain->AddFileInfoList(collection.GetList());
 		return (TTree*) tempChain;
+	}
+
+	/**
+	 * @brief My cut benchmarks.
+	 */
+	namespace cuts {
+
+		double sigBkgRatio(double sig, double bkg) {
+			return sig * sig / (bkg + sig);
+		}
+		/**
+		 * @brief Cut benchmark. For list of limits
+		 * @details Finds the best signal to background ratio for a quantity and several limits, returns a graph of the ratios.
+		 * Internal verbose parameter needs to be switched on manually.
+		 * 
+		 * There are different version of this method, for different uses cases. Usually only the steps parameter is different, e.g. sometimes a list of values, sometimes a range. The working principle of all methods is the same, though.
+		 * 
+		 * @param treeSig TTree holding testCut quantity to benchmark for SIGNAL
+		 * @param treeBkg TTree holding testCut quantity to benchmark for BACKGROUND
+		 * @param scaleFactorBkg Value with which the background is scaled
+		 * @param testCut The cut which is tested, e.g. 'mass'
+		 * @param defaultCut A pre-cut applied to all measurements of entries. Choose an empty string, if you don't need to apply any cuts. I'd like to define a TCut cutNone = ""
+		 * @param steps Vector of quantities, e.g. {1, 2, 3}
+		 * @param bestRatio The best ratio will be filled into this variable
+		 * @return Graph with the S/B ratio for every evaluated point
+		 */
+		TGraph * cutBenchmark(TTree * treeSig, TTree * treeBkg, double scaleFactorBkg, TCut testCut, TCut defaultCut, std::vector<double> steps, double & bestRatio) {
+			TGraph * tempGraph = new TGraph();
+			int iPoint = 0;
+			double lastRatio = -1.;
+			bool verbose = false;
+			for (std::vector<double>::iterator it = steps.begin(); it != steps.end(); it++, iPoint++)	{
+				TCut currentCut = TString::Format("%s > %f", (char*) testCut, *it);
+				if (verbose) std::cout << (char*)currentCut << std::endl;
+				double entriesSig = treeSig->GetEntries(currentCut && defaultCut);
+				if (verbose) std::cout << entriesSig << std::endl;
+				double entriesBkg = scaleFactorBkg * treeBkg->GetEntries(currentCut && defaultCut);
+				if (verbose) std::cout << entriesBkg << std::endl;
+				double ratio = sigBkgRatio(entriesSig, entriesBkg);
+				if (verbose) std::cout << ratio << std::endl;
+				if (ratio > lastRatio) {
+					bestRatio = *it;
+					lastRatio = ratio;
+				}
+				tempGraph->SetPoint(iPoint, *it, ratio);
+			}
+			return tempGraph;
+		}
+		/**
+		 * @brief Cut benchmark. For a simple upper limit and lower limit. Deprecated, as this can also be done with the other functions.
+		 **/
+		TGraph * cutBenchmark(TTree * treeSig, TTree * treeBkg, double scaleFactorBkg, TCut testCut, TCut defaultCut, std::pair<double, double> rangeLimits, Int_t numberOfSteps) {
+			TGraph * tempGraph = new TGraph();
+			double stepWidth = (rangeLimits.second - rangeLimits.first) / (double)numberOfSteps;
+			int iPoint = 0;
+			bool verbose = false;
+			for (double i = rangeLimits.first; i < rangeLimits.second; i += stepWidth, iPoint++)	{
+				if (verbose) std::cout << i << std::endl;
+				TCut currentString = TString::Format("%s > %f", (char*) testCut, i);
+				TCut currentCut = currentString;
+				if (verbose) std::cout << (char*)currentCut << std::endl;
+				double entriesSig = treeSig->GetEntries(currentCut && defaultCut);
+				if (verbose) std::cout << entriesSig << std::endl;
+				double entriesBkg = scaleFactorBkg * treeBkg->GetEntries(currentCut && defaultCut);
+				if (verbose) std::cout << entriesBkg << std::endl;
+				double ratio = sigBkgRatio(entriesSig, entriesBkg);
+				if (verbose) std::cout << ratio << std::endl;
+				tempGraph->SetPoint(iPoint, i, ratio);
+			}
+			return tempGraph;
+		}
+		/**
+		 * @brief Cut benchmark. For a symmetrical range around a central value.
+		 **/
+		TGraph * cutBenchmarkSymmetric(TTree * treeSig, TTree * treeBkg, double scaleFactorBkg, TCut testCut, TCut defaultCut, double centralValue, double rangeLimit, Int_t numberOfSteps, double & _lowerLimit, double & _upperLimit) {
+			TGraph * tempGraph = new TGraph();
+			double stepWidth = rangeLimit / (double)numberOfSteps;
+			double lastRatio = -1.;
+			bool verbose = false;
+			for (int i = 1; i <= numberOfSteps; i++)	{
+				if ((i % 100) == 0) std::cout << "Loop: " << i << std::endl;
+				double upperLimit = centralValue + i * stepWidth;
+				double lowerLimit = centralValue - i * stepWidth;
+				TCut currentCut = TString::Format("%s > %f && %s < %f", (char*) testCut, lowerLimit, (char*) testCut, upperLimit);
+
+				if (verbose) std::cout << "Cut: " << (char*)currentCut << std::endl;
+				double entriesSig = treeSig->GetEntries(currentCut && defaultCut);
+				if (verbose) std::cout << "#Sig: " << entriesSig << std::endl;
+				double entriesBkg = scaleFactorBkg * treeBkg->GetEntries(currentCut && defaultCut);
+				if (verbose) std::cout << "#Bkg: " << entriesBkg << std::endl;
+				double ratio = sigBkgRatio(entriesSig, entriesBkg);
+				if (verbose) std::cout << "Ratio: " << ratio << std::endl;
+				if (verbose) std::cout << "upperLimit: " << upperLimit << std::endl;
+				if (entriesSig == 0) ratio = 0;
+				if (entriesBkg == 0) ratio = 0;
+				if (ratio > lastRatio) {
+					_lowerLimit = lowerLimit, _upperLimit = upperLimit;
+					lastRatio = ratio;
+				}
+				tempGraph->SetPoint(i - 1, upperLimit - lowerLimit, ratio);
+				
+			}
+			return tempGraph;
+		}
 	}
 
 	// Following functions are for my specific analysis
