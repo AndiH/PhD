012a83e3e592cb829c4109f8a57d7e8b1e8ab694
AndiH <a.herten+github@gmail.com>
2015-07-30 10:27:54 +0200

    Updated docu

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['doublegaussfit',['doubleGaussFit',['../namespaceandi.html#a4c4d7495c52f889f1f38e7bfc88ba021',1,'andi::doubleGaussFit(TH1 *hist, bool verbose=false, bool useAutoRange=true, double innerRangeMax=0.05, double outerRangeMax=0.3)'],['../namespaceandi.html#a82adbb8f1f1d4f7c081b121be36f42f3',1,'andi::doubleGaussFit(TH1 *hist, bool verbose, double rangeStart, double rangeEnd, double *startParameters)']]],
+  ['doublegaussfitexcludecenter',['doubleGaussFitExcludeCenter',['../namespaceandi.html#a6e997c16c1722a2b1f76274849150710',1,'andi']]],
   ['doublegaussfitnonzero',['doubleGaussFitNonZero',['../namespaceandi.html#a71b889f883a5af64f356229350248820',1,'andi']]],
   ['doublegausstotwogauss',['doubleGaussToTwoGauss',['../namespaceandi.html#a3b04ea633b8ad1f0c70dfa10122d33ac',1,'andi']]]
 ];
